e90dc9dbe6e124b56f1086df391b249cee4856da
Effortless <30777722+effortlessdevsec@users.noreply.github.com>
2021-01-18 12:09:28 +0530

    Add files via upload

---
+++ files/spring-boot-fileupload/src/main/java/com/javainuse/controllers/FileUploadController.java
@@ -0,0 +1,46 @@
+package com.javainuse.controllers;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class FileUploadController {
+
+	private static String UPLOAD_FOLDER = "C://test//";
+
+	@RequestMapping("/upload")
+	public ModelAndView showUpload() {
+		return new ModelAndView("upload");
+	}
+
+	@PostMapping("/upload")
+	public ModelAndView fileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+
+		if (file.isEmpty()) {
+			return new ModelAndView("status", "message", "Please select a file and try again");
+		}
+
+		try {
+			// read and write the file to the slelected location-
+			byte[] bytes = file.getBytes();
+			Path path = Paths.get(UPLOAD_FOLDER + file.getOriginalFilename());
+			Files.write(path, bytes);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return new ModelAndView("status", "message", "File Uploaded sucessfully");
+	}
+
+}
